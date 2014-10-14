6ca2708a4e73332f862310145f22c230f4f59065
Kruglov Kirill <kruglov@intaro.ru>
2014-10-14 16:24:21 +0400

    Function defined added.

---
+++ DQL/DefinedFunction.php
@@ -0,0 +1,29 @@
+<?php
+namespace Intaro\HStoreBundle\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Configuration;
+
+class DefinedFunction extends FunctionNode
+{
+    public $hstoreExpression = null;
+    public $keyExpression = null;
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'defined('.
+            $this->hstoreExpression->dispatch($sqlWalker) . ',' .
+            $this->keyExpression->dispatch($sqlWalker) . ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->hstoreExpression = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->keyExpression = $parser->StringPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
