cc398b43ab98d4cc10b2d673bef1e8bbc73b1c86
1on <ion44@yandex.ru>
2014-12-16 13:55:48 +0300

    Fixed type mapping

--- IntaroHStoreBundle.php
+++ IntaroHStoreBundle.php
@@ -6,10 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IntaroHStoreBundle extends Bundle
 {
-    public function boot()
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $platform = $em->getConnection()->getDatabasePlatform();
-        $platform->registerDoctrineTypeMapping('hstore', 'hstore');
-    }
 }
